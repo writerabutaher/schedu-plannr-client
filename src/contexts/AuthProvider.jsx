c8c24ae41b109741282074a02bc9e99b2d16b6f7
Abu Taher <writerabutaher@gmail.com>
2023-05-03 10:13:24 +0600

    authprovider

--- src/contexts/AuthProvider.jsx
+++ src/contexts/AuthProvider.jsx
@@ -1,14 +1,86 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 import app from '../firebase/firebase.config'
-import { getAuth } from "firebase/auth"
+import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth } from "firebase/auth"
 
 const auth = getAuth(app);
 
 export const AuthContext = createContext();
 
-const AuthProvider = () => {
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true)
+    const googleProvider = new GoogleAuthProvider;
+
+    // Register User
+    const RegisterUser = async (email, password, displayName) => {
+        setLoading(true);
+        const userCredential = await createUserWithEmailAndPassword(
+            auth,
+            email,
+            password
+        );
+
+        const newUser = {
+            email: email,
+            displayName: displayName
+        };
+
+        setUser(newUser);
+
+        updateProfile(userCredential.user, {
+            displayName: displayName,
+        })
+            .then(() => { })
+            .catch((err) => console.error(err));
+    };
+
+    // google sign in
+    const googleSignIn = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    };
+
+    // log in user
+    const logInUser = async (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    };
+
+    // sign out
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth)
+            .then(() => { })
+            .catch(err => console.error(err));
+    };
+
+    // user observer
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (current) => {
+            setUser(current);
+            setLoading(false);
+        });
+        return () => unsubscribe();
+    }, []);
+
+    // Reset password
+    const resetPassword = (email) => {
+        return sendPasswordResetEmail(auth, email);
+    };
+
+    const values = {
+        RegisterUser,
+        logInUser,
+        logOut,
+        googleSignIn,
+        resetPassword,
+        loading,
+        user
+    }
+
     return (
-        <div>AuthProvider</div>
+        <AuthContext.Provider value={values}>{children}</AuthContext.Provider>
     )
 }
 
